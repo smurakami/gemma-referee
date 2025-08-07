11be754125407aadfadfc4c5ab1576b434ac59c7
s.murakami <smurakami.84@gmail.com>
2025-08-07 11:07:16 +0900

    light weight model

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -4,6 +4,8 @@ import {create} from 'zustand';
 export interface AppStoreData {
   lang: 'ja'|'en';
   setLang: (lang: "ja"|"en") => void;
+
+  
 }
 
 export const useAppStore = create<AppStoreData>(set => ({
